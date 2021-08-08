5aaccd45249c29d0513b6a1100a1a8b02490c3d2
SEHC <Zinh Nguyen>
2021-08-08 23:27:57 +0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,18 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:nhinh_uit_devera/routes/Walkthrough.dart';
+import 'package:nhinh_uit_devera/routes/tasks_screen.dart';
+import '';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage('images/Fill 1.png'),
-              ),
-              Image.asset('images/aking.png'),
-              Text(
-                'Devera',
-                style: TextStyle(
-                  fontFamily: 'pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Yeye',
-                style: TextStyle(
-                    fontFamily: 'AvenirNextRoundedPro',
-                    fontSize: 48.0,
-                    fontWeight: FontWeight.bold,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 10.0,
-                        color: Colors.black38,
-                        offset: Offset(0.0, 3.0),
-                      ),
-                    ]),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.teal.shade100,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 50.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    size: 20.0,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+84 989 914 175',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '19522596@gm.uit.edu.vn',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      color: Colors.teal.shade900,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: TasksScreen(),
     );
   }
 }
+
---
+++ lib/routes/Walkthrough.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WalkThrough extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
